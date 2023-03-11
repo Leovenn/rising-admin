411e7da2069f7f3643f1568121a308f3b176bb9c
leoven <leovenn@163.com>
2023-03-11 17:09:22 +0800

    🎉 init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="a">
+    <div class="b">fdasf</div>
+    <el-input>132</el-input>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { reactive, toRaw } from 'vue'
+interface StateType {}
+const state: StateType = reactive({})
+console.log(JSON.stringify(toRaw(state)))
+</script>
+
+<style lang="less" scoped></style>
