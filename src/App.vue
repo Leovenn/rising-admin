6719e6d39b4795b7ca45102c0645ba9949db2589
leoven <leovenn@163.com>
2023-03-12 16:45:19 +0800

    🐎 ci : ts配置相关 viteconfig 自动导入

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="a">
     <div class="b">fdasf</div>
-    <el-input>132</el-input>
+    <el-input v-model="a">132</el-input>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { reactive, toRaw } from 'vue'
-interface StateType {}
-const state: StateType = reactive({})
-console.log(JSON.stringify(toRaw(state)))
+const a = ref('1')
 </script>
 
 <style lang="less" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,10 +12,23 @@ export default defineConfig({
     AutoImport({
       resolvers: ElementPlusResolver(),
       dts: 'src/typings/auto-imports.d.ts',
+      include: [
+        /\.tsx?$/, // .ts, .tsx
+        /\.vue$/,
+      ],
+      imports: ['vue', 'vue-router'],
     }),
     Components({
       resolvers: [ElementPlusResolver()],
       dts: 'src/typings/components.d.ts',
     }),
   ],
+
+  base: './',
+
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 })
