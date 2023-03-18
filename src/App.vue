b2e6563ac535d67f967c03c5f5a7fec39434917b
leoven <leovenn@163.com>
2023-03-18 13:11:25 +0800

    🔧 build: 新增unocss

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,32 @@
   <div class="a">
     <div class="b">fdasf</div>
     <el-input v-model="a">132</el-input>
+    <div class="c-yellow flex-1 bg-#666">
+      <div>1</div>
+      <div>2</div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 const a = ref('1')
+
+const b = reactive({
+  a,
+})
+
+const c1 = reactive({
+  a: 1,
+})
+
+const c2 = toRefs(c1)
+
+console.log(JSON.stringify(c1))
+c2.a.value = 4
+
+console.log(c1.a === c2.a.value)
+
+console.log(JSON.stringify(b))
 </script>
 
 <style lang="less" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { resolve } from 'path'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,6 +23,8 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
       dts: 'src/typings/components.d.ts',
     }),
+
+    UnoCSS(),
   ],
 
   base: './',
