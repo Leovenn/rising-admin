16262435da69803b9f9a8aa22bd90b73f363b281
leoven <leovenn@163.com>
2023-03-18 16:14:06 +0800

    ✨ feat: reset.css

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import '@unocss/reset/normalize.css'
+import '@/styles/css/reset.css'
 import 'uno.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.mount('#app')
