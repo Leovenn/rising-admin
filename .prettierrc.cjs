89ec87c7c412afd1486fbff0a06aaf9a80f2ba8f
leoven <leovenn@163.com>
2023-07-01 16:53:53 +0800

    ✨ feat: prettier config

---
+++ .prettierrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  // 使用单引号而不是双引号
+  singleQuote: true,
+
+  // 在语句末尾打印分号
+  semi: false,
+
+  tabWidth: 2,
+
+  printWidth: 200,
+
+  // 在多行逗号分隔的语法结构中尽可能打印尾随逗号
+  trailingComma: 'all',
+
+  // 在 JSX 中使用单引号而不是双引号
+  jsxSingleQuote: true,
+
+  // 将>多行 HTML（HTML、JSX、Vue、Angular）元素的 放在最后一行的末尾，而不是单独放在下一行（不适用于自闭合元素）。
+  bracketSameLine: true,
+}
