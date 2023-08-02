c6779da5750d5bfcfda5f13d977c58b7c3035f9d
leoven <leovenn@163.com>
2023-08-02 20:49:21 +0800

    🔧 build: update

--- src/chrome-tab.vue
+++ src/chrome-tab.vue
@@ -1,115 +1,130 @@
 <template>
-  <div class="chrome-tab h-full flex c-[var(--el-text-color-primary)] text-14px p-[10px_10px_0_10px]">
-    <div class="chrome-tab-item active">
-      <div class="flex items-center gap-10px">
-        <div class="icon">
-          <i-line-md:heart-filled />
-        </div>
-        <div class="title">工作台</div>
-        <div class="i-line-md:close-small hover:i-line-md:close-circle"></div>
-      </div>
-    </div>
-
-    <div class="chrome-tab-item">
-      <div class="flex items-center gap-10px">
-        <div class="icon">
-          <!-- <i-line-md:close-small /> -->
-          <i-line-md:moon-twotone />
-        </div>
-        <div class="title">分分析页分析页分析页析页</div>
-        <div class="i-line-md:close-small hover:i-line-md:close-circle"></div>
-      </div>
-    </div>
-
-    <div class="chrome-tab-item">
-      <div class="flex items-center gap-10px">
-        <div class="icon">
-          <i-line-md:linkedin />
-        </div>
-        <div class="title">仪表盘</div>
-        <div class="i-line-md:close-small hover:i-line-md:close-circle"></div>
+  <div class="chrome-tab">
+    <div class="chrome-tab-item" v-for="item in list">
+      <div class="chrome-tab-item-content">
+        <div class="icon"><component :is="item.icon" /></div>
+        <div class="title">{{ item.title }}</div>
+        <div class="i-line-md:close-small hover:i-line-md:close-circle" v-if="item.isClose"></div>
       </div>
     </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const list = ref([
+  {
+    title: '工作台',
+    icon: 'i-line-md:heart-filled',
+    isClose: true,
+  },
+
+  {
+    title: '分析页',
+    icon: 'i-line-md:moon-twotone',
+    isClose: true,
+  },
+
+  {
+    title: '仪表盘',
+    icon: 'i-line-md:linkedin',
+    isClose: true,
+  },
+])
+</script>
 
 <style lang="less" scoped>
-.chrome-tab-item {
-  position: relative;
+// 弧形
+.rc (@bg-color) {
+  background-color: @bg-color;
 
-  width: fit-content;
+  &::before {
+    background-image: radial-gradient(circle 20px at 100% 0, transparent 10px, @bg-color 50%);
+  }
+  &::after {
+    background-image: radial-gradient(circle 10px at 0 0, transparent 10px, @bg-color 50%);
+  }
+}
+
+.flex-item-center-relative () {
+  position: relative;
 
   display: flex;
 
   align-items: center;
+}
 
-  padding: 10px;
+.chrome-tab {
+  height: 100%;
 
-  cursor: pointer;
+  display: flex;
 
-  border-radius: 10px 10px 0 0;
+  color: var(--el-text-color-primary);
+
+  font-size: 14px;
+
+  padding: 10px 10px 0 10px;
 
   user-select: none;
 
-  & > div {
-    position: relative;
+  .chrome-tab-item {
+    .flex-item-center-relative ();
 
-    &::after {
-      content: '|';
-      position: absolute;
-      right: -13px;
-    }
-  }
+    width: fit-content;
 
-  &.active,
-  &:hover {
-    > div {
+    padding: 10px;
+
+    cursor: pointer;
+
+    border-radius: 10px 10px 0 0;
+
+    .chrome-tab-item-content {
+      .flex-item-center-relative ();
+
+      gap: 10px;
+
       &::after {
-        content: '';
+        content: '|';
+        position: absolute;
+        right: -13px;
       }
     }
-  }
 
-  &::before,
-  &::after {
-    content: '';
-    position: absolute;
-    bottom: 0;
-    width: 10px;
-    height: 10px;
-  }
-
-  &::before {
-    right: -10px;
-  }
-
-  &::after {
-    left: -10px;
-  }
-
-  &:hover {
-    background-color: var(--el-menu-text-color-1);
+    &::before,
+    &::after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      width: 10px;
+      height: 10px;
+    }
 
     &::before {
-      background-image: radial-gradient(circle 20px at 100% 0, transparent 10px, var(--el-menu-text-color-1) 50%);
+      right: -10px;
     }
-    &::after {
-      background-image: radial-gradient(circle 10px at 0 0, transparent 10px, var(--el-menu-text-color-1) 50%);
-    }
-  }
 
-  &.active {
-    color: var(--primary-color);
-    background-color: var(--primary-color-4);
-    z-index: 1;
-
-    &::before {
-      background-image: radial-gradient(circle 20px at 100% 0, transparent 10px, var(--primary-color-4) 50%);
-    }
     &::after {
-      background-image: radial-gradient(circle 10px at 0 0, transparent 10px, var(--primary-color-4) 50%);
+      left: -10px;
+    }
+
+    &.active,
+    &:hover {
+      .chrome-tab-item-content {
+        &::after {
+          content: '';
+        }
+      }
+    }
+
+    &:hover {
+      .rc(var(--el-menu-text-color-1));
+    }
+
+    &.active {
+      color: var(--primary-color);
+
+      z-index: 1;
+
+      .rc(var(--primary-color-4));
     }
   }
 }
